d836b9f2c518aeb3ecb8814e9d1ee6b96b7e00f6
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2014-08-18 17:24:30 -0400

    POC3

---
+++ google-feed-api/POC3/POC3/app.js
@@ -0,0 +1,7 @@
+﻿var request = require('request');
+
+request('http://sxp.microsoft.com/feeds/3.0/msdntn/CSharpHeadlines', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Print the google web page.
+    }
+})
