fd4cb46f5ad390c6dd3bdcbfacb641557c978183
PinalBhattLive <Pinal-Bhatt@live.com>
2014-08-30 07:48:43 -0400

    express serving static files favicon.ico added

--- MEAN/server.js
+++ MEAN/server.js
@@ -6,6 +6,8 @@ var app = express();
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('*', function(req, res){
     res.render('index');
 });
