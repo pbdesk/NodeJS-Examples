0cf7486b5b09ae49548d070c7f502742e77bdf1a
PinalBhattLive <Pinal-Bhatt@live.com>
2014-08-30 11:50:19 -0400

    mongoose connect

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var env  = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -9,6 +10,14 @@ app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
+
+/*Mongoose/Mongo connect*/
+mongoose.connect('mongodb://localhost/MeanDB')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('MeanDB db opened');
+});
 
 app.get('/ngPartials/:partialPath',function(req, res){
     res.render('ngPartials/' + req.params.partialPath);
