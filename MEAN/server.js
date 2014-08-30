33b58361b11ccb24d9f178740ad269557a2b86d9
PinalBhattLive <Pinal-Bhatt@live.com>
2014-08-30 10:36:47 -0400

    layout.jade referencing css & js scripts via cdn

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var env  = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
-
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res){
